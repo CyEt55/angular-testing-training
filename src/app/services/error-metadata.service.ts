a076302906e93630aa9291f952c29d7c94c3ea22
CyEt55 <albertz_2011@hotmail.com>
2023-05-22 13:00:32 -0500

    added custom error handler and let an error occur to test

--- src/app/services/error-metadata.service.ts
+++ src/app/services/error-metadata.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ErrorHandler } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorMetadataService {
+export class ErrorMetadataService implements ErrorHandler {
+  
+  public handleError(error: any): void {
+    const date = new Date();
 
-  constructor() { }
+    console.error({
+      timestanp: date.toISOString(),
+      message: error.message,
+      zone: error.zone
+    });
+  }
 }
