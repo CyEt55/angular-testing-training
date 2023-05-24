8ab71de5da4c7dae369c7340ed7415eaa50963b1
CyEt55 <albertz_2011@hotmail.com>
2023-05-24 11:14:24 -0500

    had to look the solution D:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { UserListComponent } from './user-list/user-list.component';
 
 import { ErrorMetadataService } from './services/error-metadata.service';
 import { HighlightTextPipe } from './pipes/highlight-text.pipe';
+import { UserFilterPipe } from './pipes/user-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
-    HighlightTextPipe
+    HighlightTextPipe,
+    UserFilterPipe
   ],
   imports: [
     BrowserModule,
